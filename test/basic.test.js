2cefc12c1e2d7739c09af9ab48a62d0f4dfba8a5
Tuan Anh Tran <me@tuananh.org>
2017-06-03 13:16:45 +0700

    add test unit and travis ci

---
+++ test/basic.test.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const test = require('tape')
+const transform = require('../')
+
+test('basic test', (t) => {
+    const xml = fs.readFileSync('examples/ean.xml', 'utf-8')
+    const template = {
+        cache_key: "/HotelListResponse/cacheKey",
+        hotels: ["//HotelSummary", {
+            hotel_id: "hotelId",
+            name: "name",
+            rooms: ["RoomRateDetailsList/RoomRateDetails", {
+                rates: ["RateInfos/RateInfo", {
+                    currency: "ChargeableRateInfo/@currencyCode",
+                    non_refundable: "nonRefundable",
+                    price: "ChargeableRateInfo/@total"
+                }],
+                room_name: "roomDescription",
+                room_type_id: "roomTypeCode"
+            }]
+        }],
+        session_id: "/HotelListResponse/customerSessionId"
+    }
+
+    const result = transform(xml, template)
+    t.equal(typeof result, 'object', 'result is expected to be object')
+    t.equal(result.cache_key, '-48a4e19f:15bec159775:50eb', 'parse cache_key ok')
+    t.equal(result.session_id, 'yuvb3jdpifp2t13y43pass2p', 'parse session_id ok')
+    t.equal(Array.isArray(result.hotels), true, 'result.hotels is expected to be array')
+
+    result.hotels.forEach(h => {
+        t.test(`checking node [hotel_id= ${h.hotel_id}]`, function(tt) {
+            tt.ok(h.hotel_id, `node [hotel_id=${h.hotel_id}] has hotel_id`)
+            tt.ok(h.name, `node [hotel_id=${h.hotel_id}] has name`)
+            tt.end()
+        })
+    })
+
+    t.end()
+})
