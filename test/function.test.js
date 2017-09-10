112c8b66585fe9bf1e0379494ff2c485525c4ccd
Tuan Anh Tran <me@tuananh.org>
2017-09-11 05:59:14 +0700

    feat: add snake-case and camel-case functions
    * add snake-case and camel-case functions
    * update test cases
    * update readme

--- test/function.test.js
+++ test/function.test.js
@@ -8,10 +8,10 @@ const xml = `
         <number>20.3</number>
         <boolean>TrUe</boolean>
         <items>
-            <item>hEllo WorlD</item>
-            <item>Hello World</item>
-            <item>hello wOrld</item>
-            <item>hEllO wOrlD</item>
+            <item>hEllo WorlD camaRo</item>
+            <item>Hello World cAmaro</item>
+            <item>hello wOrld caMaro</item>
+            <item>hEllO wOrlD camaRo</item>
         </items>
     </root>
 `
@@ -21,7 +21,7 @@ t.test('test function upper-case()', (t) => {
         upperCase: ['//items/item', 'upper-case(.)']
     })
     result.upperCase.forEach(u => {
-        t.equal(u, 'HELLO WORLD')
+        t.equal(u, 'HELLO WORLD CAMARO')
     })
     t.end()
 })
@@ -31,7 +31,7 @@ t.test('test function lower-case()', (t) => {
         lowerCase: ['//items/item', 'lower-case(.)']
     })
     result.lowerCase.forEach(u => {
-        t.equal(u, 'hello world')
+        t.equal(u, 'hello world camaro')
     })
     t.end()
 })
@@ -41,17 +41,17 @@ t.test('test function title-case()', (t) => {
         titleCase: ['//items/item', 'title-case(.)']
     })
     result.titleCase.forEach(u => {
-        t.equal(u, 'Hello World')
+        t.equal(u, 'Hello World Camaro')
     })
     t.end()
 })
 
-t.test('test fubuildnction camel-case()', (t) => {
+t.test('test function camel-case()', (t) => {
     const result = transform(xml, {
         camelCase: ['//items/item', 'camel-case(.)']
     })
     result.camelCase.forEach(u => {
-        t.equal(u, 'helloWorld')
+        t.equal(u, 'helloWorldCamaro')
     })
     t.end()
 })
@@ -61,7 +61,7 @@ t.test('test function snake-case()', (t) => {
         snakeCase: ['//items/item', 'snake-case(.)']
     })
     result.snakeCase.forEach(u => {
-        t.equal(u, 'hello_world')
+        t.equal(u, 'hello_world_camaro')
     })
     t.end()
 })
