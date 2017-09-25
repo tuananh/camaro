df61156f8e0f995a32495d09f9ab6ce3b5fb8d3f
Tuan Anh Tran <me@tuananh.org>
2017-09-25 20:29:56 +0700

    test: more test

--- test/basic.test.js
+++ test/basic.test.js
@@ -12,8 +12,8 @@ t.test('basic test', (t) => {
             rooms: ['RoomRateDetailsList/RoomRateDetails', {
                 rates: ['RateInfos/RateInfo', {
                     currency: 'ChargeableRateInfo/@currencyCode',
-                    non_refundable: 'nonRefundable',
-                    price: 'ChargeableRateInfo/@total'
+                    non_refundable: 'boolean(nonRefundable = "true")',
+                    price: 'number(ChargeableRateInfo/@total)'
                 }],
                 room_name: 'roomDescription',
                 room_type_id: 'roomTypeCode'
@@ -29,13 +29,18 @@ t.test('basic test', (t) => {
     t.equal(result.cache_key, '-48a4e19f:15bec159775:50eb', 'parse cache_key ok')
     t.equal(result.session_id, 'yuvb3jdpifp2t13y43pass2p', 'parse session_id ok')
     t.equal(Array.isArray(result.hotels), true, 'result.hotels is expected to be array')
-    t.equal(result.path_not_exist, "", 'path_not_exist should be empty string')
+    t.equal(result.path_not_exist, '', 'path_not_exist should be empty string')
     t.ok(result.empty_array && Array.isArray(result.empty_array), 'empty_array should be an empty array')
 
     result.hotels.forEach(h => {
         t.test(`checking node [hotel_id= ${h.hotel_id}]`, function(tt) {
             tt.ok(h.hotel_id, `node [hotel_id=${h.hotel_id}] has hotel_id`)
             tt.ok(h.name, `node [hotel_id=${h.hotel_id}] has name`)
+            tt.ok(Array.isArray(h.rooms), `node [hotel_id=${h.hotel_id}] has rooms array`)
+            h.rooms.forEach(room => {
+                tt.ok(room.room_name, 'room_name is there')
+                tt.ok(room.room_type_id, 'room_type_id is there')
+            })
             tt.end()
         })
     })
