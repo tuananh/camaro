a2b9b5e4f01f109c9ac73d2e6b6fd76f87e84018
Tuan Anh Tran <me@tuananh.org>
2017-10-06 08:29:04 +0700

    test: update test

--- test/array-in-array.test.js
+++ test/array-in-array.test.js
@@ -1,50 +1,41 @@
 const t = require('tape')
 const transform = require('../')
 
-const xml =
-`
-    <root>
-        <arrayOuter>
-            <arrayInner>
-                <item>outer0.arr0.0</item>
-                <item>outer0.arr0.1</item>
-                <item>outer0.arr0.2</item>
-            </arrayInner>
-            <arrayInner>
-                <item>outer0.arr1.0</item>
-                <item>outer0.arr1.1</item>
-                <item>outer0.arr1.2</item>
-            </arrayInner>
-        </arrayOuter>
-        <arrayOuter>
-            <arrayInner>
-                <item>outer1.arr0.0</item>
-                <item>outer1.arr0.1</item>
-                <item>outer1.arr0.2</item>
-            </arrayInner>
-            <arrayInner>
-                <item>outer1.arr1.0</item>
-                <item>outer1.arr1.1</item>
-                <item>outer1.arr1.2</item>
-            </arrayInner>
-        </arrayOuter>
-    </root>
+const xml = `
+    <element>
+        <item>outer0.arr0.0</item>
+        <item>outer0.arr0.1</item>
+        <item>outer0.arr0.2</item>
+    </element>
+    <element>
+        <item>outer0.arr1.0</item>
+        <item>outer0.arr1.1</item>
+        <item>outer0.arr1.2</item>
+    </element>
+    <element>
+        <item>outer1.arr0.0</item>
+        <item>outer1.arr0.1</item>
+        <item>outer1.arr0.2</item>
+    </element>
+    <element>
+        <item>outer1.arr1.0</item>
+        <item>outer1.arr1.1</item>
+        <item>outer1.arr1.2</item>
+    </element>
 `
 
-t.test('array-in-array test', (t) => {
+t.test('array-in-array test .// should only match nodes inside current node', (t) => {
     const template = {
-        arrayOuter: ['//arrayOuter', {
-            arrayInner: ['//arrayInner', {
-                item: 'item'
-            }]
+        elements: ['//element', {
+            items: ['.//item', '.']
         }]
     }
 
     const result = transform(xml, template)
-    // console.log(JSON.stringify(result, null, 4))
-    t.equal(result.arrayOuter[0].arrayInner.length, 2, 'inner should have only 2 elements')
-    t.equal(result.arrayOuter[0].arrayInner[0].item, 'outer0.arr0.0', 'outer0.arr0.0')
-    t.equal(result.arrayOuter[0].arrayInner[1].item, 'outer0.arr0.1', 'outer0.arr0.1')
+    t.equal(result.elements[0].items.length, 3, 'elements[0].items should have only 3 elements')
+    t.equal(result.elements[0].items[0], 'outer0.arr0.0', 'outer0.arr0.0')
+    t.equal(result.elements[0].items[1], 'outer0.arr0.1', 'outer0.arr0.1')
+    t.equal(result.elements[0].items[2], 'outer0.arr0.2', 'outer0.arr0.2')
 
     t.end()
 })
