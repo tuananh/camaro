b46afb2decf377b299452608cf161ce24873b776
Tuan Anh Tran <me@tuananh.org>
2019-05-30 13:53:16 +0700

    add pretty-data to benchmark

--- benchmark/pretty-print.js
+++ benchmark/pretty-print.js
@@ -1,6 +1,7 @@
 const benchmark = require('benchmark')
 const fs = require('fs')
 const { prettyPrint } = require('../')
+const { pd } = require('pretty-data')
 
 const suite = new benchmark.Suite()
 const xml = fs.readFileSync('examples/ean.xml', 'utf-8')
@@ -8,6 +9,10 @@ const xml = fs.readFileSync('examples/ean.xml', 'utf-8')
 suite.add('camaro prettyPrint()', function(deferred) {
     prettyPrint(xml).then(_ => deferred.resolve())
 }, { defer: true })
+
+suite.add('pretty-data', function () {
+    pd.xml(xml)
+})
 
 suite.on('cycle', cycle)
 suite.run()
