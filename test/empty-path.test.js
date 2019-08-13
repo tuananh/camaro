4792263ca83dabf237968d417c0af13a1255a5cc
Tuan Anh Tran <me@tuananh.org>
2019-08-13 08:57:58 +0700

    fix: case where path is empty

---
+++ test/empty-path.test.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const t = require('tape')
+const { transform } = require('../')
+
+t.test('empty path test', async t => {
+    const xml = '<hello>world</hello>'
+    const template = {
+        empty: ''
+    }
+
+    const result = await transform(xml, template)
+    t.equal(result.empty,'','empty path => empty string')
+    t.end()
+})
