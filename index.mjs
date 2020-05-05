c9c5ffcb4598945ad091c474889355f726046de4
Tuan Anh Tran <me@tuananh.org>
2020-05-05 14:55:46 +0700

    fix: update nlohmann/json, support esm

---
+++ index.mjs
@@ -0,0 +1,7 @@
+import mod from "./index.js";
+
+export default mod;
+export const ready = mod.ready;
+export const transform = mod.transform;
+export const toJson = mod.toJson;
+export const prettyPrint = mod.prettyPrint;
